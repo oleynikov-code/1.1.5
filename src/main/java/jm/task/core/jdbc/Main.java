76556452303a2690605c16641e0970943381a76c
LuckyLeoF61 <93728762+LuckyLeoF61@users.noreply.github.com>
2023-03-02 14:09:14 +0300

    comittestmain

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -14,5 +14,6 @@ public class Main {
         System.out.println(userService.getAllUsers());
         userService.cleanUsersTable();
         userService.dropUsersTable();
+        //comitTest
     }
 }
